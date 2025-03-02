c5013464e6b687f21b0156b51ca39af1a5e946ef
Made By IToncek <itoncek3@itoncek.space>
2025-03-02 12:27:53 +0100

    Updating counting algorithm to separate the two parts

--- count.ps1
+++ count.ps1
@@ -1,37 +1,51 @@
 $val = "maturitni_prace.tex"
 echo "Counting character counts for $val"
 
-$chars = perl ./texcount.pl -char -brief $val
-$words = perl ./texcount.pl -brief $val
+$chars = perl ./texcount.pl -char -sub=part $val
+$words = perl ./texcount.pl -sub=part $val
 
-$sep = "\+"
+$sep = "\n"
+$sep2 = "\+"
 
-$charsplit = $chars -split $sep
-$charsintext = [int]$charsplit[0]
-$charsintitles = [int]$charsplit[1]
-$charsincaptions = [int](($charsplit[2]) -split " ")[0]
+$totalchars = 0;
 
-$wordsplit = $words -split $sep
-$wordsintext = [int]$wordsplit[0]
-$wordsintitles = [int]$wordsplit[1]
-$wordsincaptions = [int](($wordsplit[2]) -split " ")[0]
+$loop = 12..13;
 
-""
-"Characters in text (normpages) [% of requirement]"
-$charsum = $charsintext + $wordsintext
-$charsum.ToString() + " (" + ($charsum/1800).ToString() + ") " + "[" + (($charsum/(20*1800))*100) + "%]"
+$a = @()
 
-""
-"Characters in titles"
-$titlesum = $charsintitles + $wordsintitles
-$titlesum.ToString() + " (" + ($titlesum/1800).ToString() + ") " + "[" + (($titlesum/(20*1800))*100) + "%]"
+foreach ($i in $loop) {
+    $cs = $chars -split $sep
+    $charsplit = $cs[[int]$i] -split $sep2
+    $charsintext = [int]$charsplit[0]
 
-""
-"Characters in captions"
-$captionsum = $charsincaptions + $wordsincaptions
-$captionsum.ToString() + " (" + ($captionsum/1800).ToString() + ") " + "[" + (($captionsum/(20*1800))*100) + "%]"
+    $ws = $words -split $sep
+    $wordsplit = $ws[[int]$i] -split $sep2
+    $wordsintext = [int]$wordsplit[0]
+    $partName = (($charsplit[2]) -split " ")[1]
 
-""
-"Total"
-$grand_total = $charsum + $titlesum + $captionsum
-$grand_total.ToString() + " (" + ($grand_total/1800).ToString() + ") " + "[" + (($grand_total/(20*1800))*100) + "%]"
+    $charsum = $charsintext + $wordsintext
+
+    $charnp = ($charsum/1800)
+
+
+    $part = "";
+    $reqs = 0;
+
+    if ([int]$i -eq 13) {
+        $part = "Praktická část"
+        $reqs = 5;
+    } else {
+        $part = "Teoretická část"
+        $reqs = 15;
+    }
+    
+    $charp = (($charsum) / (1800*$reqs))
+
+    $totalchars += $charsum;
+    
+    $a += [pscustomobject]@{PartName = $part; CharsInText = $charsum; NormPagesFinished = "{0:f2}" -f $charnp; PercentFinished = "{0:p}" -f $charp}
+}
+
+$a += [pscustomobject]@{PartName = "<total>"; CharsInText = $totalchars; NormPagesFinished = "{0:f2}" -f ($totalchars/1800); PercentFinished = "{0:p}" -f (($totalchars) / (1800*20))}
+
+$a | Format-Table
